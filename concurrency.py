093d2b8f5cbd812cc461d2e7c611b03118612706
Alec Saunders <alecsaunders@gmail.com>
2018-10-16 15:09:10 -0600

    Fix bug with random start position

--- concurrency.py
+++ concurrency.py
@@ -30,9 +30,9 @@ class ConcurrencyCalculator():
                 data = data[:num_lines]
             if start_position == 'end':
                 data = data[-num_lines:]
-            if start_position == 'random':
-                start_int_rand = np.random.randint(1, len(data) - num_lines)
-                data = data[start_int_rand - 1: start_int_rand + num_lines - 1]
+            if start_position == 'random' and num_lines < len(data):
+                    start_int_rand = np.random.randint(1, len(data) - num_lines)
+                    data = data[start_int_rand - 1: start_int_rand + num_lines - 1]
         return data
 
     def calculate(self):
