7aa3cc137249ded9ba7074e35f0bbc77403cb7ae
Alec Saunders <alecsaunders90@gmail.com>
2019-06-06 13:39:42 -0600

    Add example query to be used as input to script

---
+++ example_query.sql
@@ -0,0 +1,16 @@
+SELECT EXTRACT(EPOCH FROM start_timestamp), EXTRACT(EPOCH FROM end_timestamp)
+FROM
+        DBMONITOR.query_requests_archive q
+        JOIN
+        (
+                SELECT DISTINCT pool_name, node_name, transaction_id, statement_id
+                FROM DBMONITOR.dc_resource_acquisitions
+        ) r
+                ON q.node_name = r.node_name
+                AND q.transaction_id = r.transaction_id
+                AND q.statement_id = r.statement_id
+WHERE
+        q.start_timestamp > CURRENT_DATE - INTERVAL '60 day'
+        AND r.pool_name = 'general'
+        AND q.request_duration_ms IS NOT NULL
+ORDER BY 1, 2;
